acce3af5673647deec3fa3d7d6487267c82a3263
freetime <freedomcoder@163.com>
2016-09-16 19:14:01 +0800

    测试点都通过

--- week2/02-线性结构3_ReversingLinkedList.c
+++ week2/02-线性结构3_ReversingLinkedList.c
@@ -1,201 +1,122 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX_NODE 100001
 
+typedef struct Node {
+	int addr;
+	int data;
+	int next;
+	struct Node *nextAddr;
+}Node;
+typedef struct Node List;
 
-typedef struct Node Node;
-typedef Node List; //双向链表
-struct Node {
-	int info[3];
-	Node *next;
-	Node *pre;
-};
-
-List *create(void); //创建包含头结点的空链表
-void read(List *L);
-/*
-
-注意：设置了几个不在链表中的节点
-在sort函数中对原list进行排序，并将不相干的节点去掉，并且重新计算节点数
-
-*/
-List *sort(List *L); //先将list排序
-void myFree(List *L); //释放list
-List *reverse(List *L);
-void print(List *L);
-
+List *reverse(List *head, int k);
+void print(List *list);
 int main(void)
 {
-	List *L = NULL;
-	List *newList = NULL;
-	List *reverseList = NULL;
+	int firstAddr, num, k, temp, i;  //firstAddr：第一个节点的位置，num：节点的个数，k：要翻转的节点数量，temp临时存储数据
+	int data[MAX_NODE], next[MAX_NODE];  //data数组为数据，索引为当前地址，next数组为下一个数据的位置，索引为当前的地址			
+	Node *head = NULL;
+	Node *reverseList = NULL;  //翻转后的链表
+	
+	scanf("%d %d %d", &firstAddr, &num, &k); //现在num读入的是input的节点数，包含了无效的节点
+	List *list = (Node*)malloc(sizeof(Node)*(num + 1));//新建一个数组链表
+	list[0].next = firstAddr;  //list的头结点中的next存储第一个数据节点的位置
+	//读入数据
+	while (num--)
+	{
+		scanf("%d", &temp);  //temp中存储的是当前节点的位置
+		scanf("%d", &data[temp]);  //data中存储的是当前位置的数据
+		scanf("%d", &next[temp]);  //next中存储的是当前节点的下一个节点的位置
+	}
+	
+	//建立有序的list	
+	i = 1; //使索引指向链表的第一个节点
+	while (1)
+	{
+		if (list[i - 1].next == -1) //通过判断上一节点的next判断是否没有节点了
+		{
+			list[i - 1].nextAddr = NULL; //因为上一节点的next为-1，所以没有下一个节点了
+			num = i-1; //num中存储的是真实有效的节点数
+			break; //为最后一个节点就跳出循环
+		}
+		list[i].addr = list[i - 1].next;  //当前节点的地址，通过上一节点的next赋值
+		list[i].data = data[list[i].addr]; //当前节点的数据，通过data数组，用当前节点的地址作为索引
+		list[i].next = next[list[i].addr];//下一个节点的地址，通过next数组，用当前节点的地址作为索引
+		list[i-1].nextAddr = list + i;  //上一个节点中存储的当前节点的真实的内存地址
+		
+		i++;
+	}
+	
+	//翻转链表
+	if (k <= num)
+	{
+		head = list; //head指向链表头结点
+		for (i = 0; i < num / k; i++)   //i在这里是计算要翻转的次数 
+		{
+			head = reverse(head, k);  //head储存的是下一段要翻转链表的头结点
+		}
+	}
+	
 
-	L = create();
-	read(L);
-	newList = sort(L);
-	myFree(L);
-	reverseList = reverse(newList);
-	print(reverseList);
+	//打印链表
+	print(list);
 	return 0;
 }
 
-List *create(void) //创建包含头结点的空链表
+
+List *reverse(List *head, int k)
 {
-	Node *head = NULL;
-
-	head = (Node*)malloc(sizeof(Node));
-	head->next = NULL;
-	head->pre = NULL;
-
-	return head;
-}
-
-void read(List *L)
-{
-	int firstAddr, n, rev;
+	int count = 1;
+	Node *pre = NULL;
+	Node *next = NULL;
 	Node *temp = NULL;
-	Node *rear = NULL;
-	rear = L;
-	
-	scanf("%d%d%d", &firstAddr, &n, &rev);
-	L->info[0] = firstAddr;
-	L->info[1] = n;
-	L->info[2] = rev;
-
-	while (n--)
+	pre = head->nextAddr; //head指向要翻转的那段链表的头结点（第一个节点的前一个节点）
+	next = pre->nextAddr;
+	while (count < k)
 	{
-		temp = (Node*)malloc(sizeof(Node));
+		temp = next->nextAddr;
+		next->nextAddr = pre;
+		next->next = pre->addr;
 
-		scanf("%d%d%d", &temp->info[0], &temp->info[1], &temp->info[2]);
-		temp->pre = rear;
-		temp->next = NULL;
+		pre = next;
+		next = temp;
 
-		rear->next = temp;
-		rear = temp;
+		count++;
 	}
 
+	head->nextAddr->nextAddr = next; //头结点的下一个节点翻转后为最后一个节点，使其指向下一段链表的第一个节点
+	if (next)  //如果next不为null
+	{
+		head->nextAddr->next = next->addr;
+	}
+	else  //next为null时，表示是pre整个链表的最后一个节点
+	{
+		head->nextAddr->next = -1;
+	}
+
+	temp = head->nextAddr;  //temp储存的是下一段要翻转的链表的头结点
+	//改变头节点的数据
+	head->nextAddr = pre;
+	head->next = pre->addr;
+	return temp; //返回翻转后的第一个节点，非头结点
 }
 
-List *reverse(List *L)
+void print(List *list)
 {
-	int K, num, tempK;
-	Node *head = NULL;
-	Node *rear = NULL;
-	Node *newHead = NULL;
-	Node *newRear = NULL;
-	Node *temp = NULL;
-
-	head = newHead= newRear = L;
-	head = head->next;  //head指向头结点
-	tempK = K = L->info[2];
-
-	num = L->info[1] / L->info[2]; //reverse的次数
-	while (num--)
+	List *p = NULL;
+	p = list->nextAddr; //指向第一个节点
+	while (p)
 	{
-		while (--tempK)
+		if (p->nextAddr)  //控制输出，如果下一个节点不为null
 		{
-			head = head->next;
+			printf("%.5d %d %.5d\n", p->addr, p->data, p->next);
 		}
-		rear = head;
-		head = head->next;
-
-		newRear->pre = rear;
-		tempK = K;
-		while (tempK--)
+		else  //下一个节点为null
 		{
-			//temp = newRear->next;
-			newRear->next = newRear->pre;  //翻转后将pre和next调换
-			newRear = newRear->next;
+			printf("%.5d %d %d", p->addr, p->data, p->next);
 		}
+		p = p->nextAddr;
 	}
-	newRear->next = head;
-
-	//对addr和next进行排序
-	rear = newHead->next;
-	while (rear->next)
-	{
-		
-		rear->info[2] = rear->next->info[0];
-		rear = rear->next;
-		
-	}
-	rear->info[2] = -1;
-
-	return newHead;
-}
-
-List *sort(List *L) //先将list排序，在sort函数中对原list进行排序，并将不相干的节点去掉，并且重新计算节点数
-{
-	int count, addr, realK;  //realK为重新计算节点数的值
-	Node *head = NULL;
-	Node *temp = NULL;
-	Node *find = NULL;
-	Node *rear = NULL;
-	head = (Node*)malloc(sizeof(Node));
-	head->info[0] = L->info[0];
-	head->info[1] = L->info[1];
-	head->info[2] = L->info[2];
-	head->next = NULL;
-	head->pre = NULL;
-	rear = head;
-	
-	realK = 0;
-	addr = L->info[0];
-	count = L->info[1];
-	find = L->next;
-	while (count-- && addr!=-1)  //将不相干的节点去掉
-	{
-		while (find->info[0] != addr && find)
-		{
-			find = find->next;
-		}
-		if (find)
-		{
-			addr = find->info[2];
-			temp = (Node*)malloc(sizeof(Node));
-			temp->info[0] = find->info[0];
-			temp->info[1] = find->info[1];
-			temp->info[2] = find->info[2];
-			temp->pre = rear;
-			temp->next = NULL;
-
-			find = L->next;
-
-			rear->next = temp;
-			rear = temp;
-
-			realK++;
-		}
-		
-
-	}
-
-	//realK为重新计算节点数的值
-	head->info[1] = realK;
-
-	
-	return head;
-}
-void myFree(List *L) //释放list
-{
-	Node *cur = NULL;
-	Node *temp = NULL;
-	cur = L;
-	while (cur)
-	{
-		temp = cur;
-		cur = cur->next;
-		free(temp);
-	}
-
-}
-void print(List *L)
-{
-	Node *cur = L->next;
-	while (cur->next)
-	{
-		printf("%05d %d %05d\n", cur->info[0], cur->info[1], cur->info[2]);
-		cur = cur->next;
-	}
-	printf("%05d %d %d", cur->info[0], cur->info[1], cur->info[2]);
 }
